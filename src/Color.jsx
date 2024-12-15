3a70c7386c5d5786deb9ff75a47140795b197d16
oltapllana <oltapllana@gmail.com>
2024-12-15 01:26:17 +0100

    first commit

--- src/Color.jsx
+++ src/Color.jsx
@@ -1,19 +1,56 @@
 import React from 'react'
-import {useState} from "react"
+import {useState , useEffect} from "react"
 const Color = () => {
-    const [color , setColor] = useState("")
+    const [color , setColor] = useState("");
+
+    const [saveColor , setSaveColor] = useState(() => {
+      const savedColors = localStorage.getItem("colorsData");
+      return savedColors ? JSON.parse(savedColors)  : []
+    });
+
+
+  
+    useEffect(() => {
+      localStorage.setItem("colorsData", JSON.stringify(saveColor));
+    }, [saveColor]);
+
+
 
     function pickColor(e){
         setColor(e.target.value)
-        console.log(color)
+        
+    }
+    function saveColorFunc(){
+      setSaveColor((prev) => [
+        ...prev ,
+        {
+          id: Math.random(),
+          text: color
+        }
+      ])
     }
 
   return (
-    <div>
+    <div className="container">
+      
+      <div className="savedColorContainer">
+        {saveColor.length > 0 && <h2>Saved Colors :</h2>}
+       {saveColor.map((color) => (
+        <div className="boxContainer"> 
+     <p>{color.text}  </p>
+     <div className="box" style={{backgroundColor: color.text}}></div>
+        </div>
+ 
+        ))}
+      </div>
+      <div className="colorPickerContainer">
       <h1>Color Picker</h1>
-      <div className="result" style={{ backgroundColor: color }}      >{color ? `Picked Color: ${color}` : "Picked Color: #FFF"}</div>
+      <div className="result" style={{ backgroundColor: color }} >{color ? `Picked Color: ${color}` : "Picked Color: #FFF"}</div>
       <h2>Pick here</h2>
-      <input type="color" onChange={(e) => pickColor(e)} />
+      <input type="color" onChange={(e) => pickColor(e)} /> <br/>
+      {color &&  <button className="saveColorButton" onClick={saveColorFunc}>Save Color</button>}
+     
+      </div>
     </div>
   )
 }
